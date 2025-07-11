a518f42b2d3e5962da971dc571e4fde7a39f445a
welcome <welcome@DESKTOP-MOMB0SM>
2025-07-11 11:52:43 +0530

    Hi

--- src/DropDown/SelectByIndex.java
+++ src/DropDown/SelectByIndex.java
@@ -1,18 +1,30 @@
 package DropDown;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import com.google.common.io.Files;
+
 public class SelectByIndex { //Topic : 8
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 
 		WebDriver driver = new ChromeDriver();
-
+         
+		String string=" ";
+		
 		driver.get("https://automationtesting.co.uk/dropdown.html");
+		
+		driver.manage().window().maximize();
+
 
 		WebElement D = driver.findElement(By.xpath("//select[@name=\"cars\"]"));
 
@@ -20,5 +32,11 @@ public class SelectByIndex { //Topic : 8
 
 		s.selectByIndex(1);
 
+		TakesScreenshot screen=(TakesScreenshot)driver;
+		File Screen=screen.getScreenshotAs(OutputType.FILE);
+		File src=new File("C:\\Users\\welcome\\git\\repository8\\Axess_Technology_Selenium_Tasks\\src"+string+"Cars.png");
+		Files.copy(Screen, src);
+		
+		System.out.println("Cars.png");
 	}
 }
